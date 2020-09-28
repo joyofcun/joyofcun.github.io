7c09a6ffe54cf18d5df5115ce8116e25d0172ebf
Anh Ph <anh.phungvan@gmail.com>
2020-09-28 16:04:47 +1000

    kk

---
+++ analytics-service.js
@@ -0,0 +1,27 @@
+function AnalyticsService({ endpoint, fetchMode } = {}) {
+    endpoint = endpoint || "https://analytics.codegewerk.de/";
+    fetchMode = fetchMode || "cors";
+  
+    this.sendData = async function(info) {
+      console.log("endpoint: ", endpoint, ", fetch-mode: ", fetchMode);
+  
+      let data = {
+        platform: navigator.platform,
+        userAgent: navigator.userAgent,
+        info: info
+      };
+  
+      const result = await fetch(endpoint, {
+        method: "POST",
+        mode: fetchMode,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+  
+      console.log(result);
+  
+      return result;
+    };
+  }
