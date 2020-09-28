9d1a5b04d72b89abd35270632534c6fa9100be5d
Anh Ph <anh.phungvan@gmail.com>
2020-09-28 16:36:36 +1000

    kk

--- analytics-service.js
+++ analytics-service.js
@@ -1,27 +1,11 @@
-function AnalyticsService({ endpoint, fetchMode } = {}) {
-    endpoint = endpoint || "https://analytics.codegewerk.de/";
-    fetchMode = fetchMode || "cors";
-  
-    this.sendData = async function(info) {
-      console.log("endpoint: ", endpoint, ", fetch-mode: ", fetchMode);
-  
-      let data = {
-        platform: navigator.platform,
-        userAgent: navigator.userAgent,
-        info: info
-      };
-  
-      const result = await fetch(endpoint, {
-        method: "POST",
-        mode: fetchMode,
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      });
-  
-      console.log(result);
-  
-      return result;
-    };
-  }
+const count = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount(){
+  fetch('https://api.countapi.xyz/update/danielazocardev/codepen/?amount=1')
+    .then(res => res.json())
+    .then(res => {
+      count.innerHTML = res.value
+  });
+}
